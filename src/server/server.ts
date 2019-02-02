4b67647570bb57566b4bed62561bbf46ff91b888
Mathieu Dumoulin <mathieu.dumoulin@outlook.com>
2019-02-02 16:35:41 -0500

    Cleaner clean up.

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,9 +1,11 @@
+const nodeCleanup = require("node-cleanup");
 import { createServer } from "http";
 import { RedisPresence, Server } from "colyseus";
 import { config } from "../global.config";
 import { RoomHandler, RoomHandlerOptions } from "./rooms";
 import { RedisAdapter } from "../core";
 import { LoadBalancerRoom } from "./load-balancer";
+import { ServerCli } from "./cli/server-cli";
 
 const http = createServer();
 const gameServer = new Server({
@@ -34,28 +36,9 @@ gameServer.register("server-info", LoadBalancerRoom);
 gameServer.matchMaker.create("server-info", {});
 
 const PORT = config.serverPort;
+let cli: ServerCli;
 
 http.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
+    cli = new ServerCli(gameServer);
 });
-
-// Cleaning up all the resources used by the server.
-function cleanUpResources() {
-    console.log("Server is closed.");
-}
-
-gameServer.onShutdown(cleanUpResources);
-
-// do something when app is closing
-/*process.on('exit', cleanUpResources);
-
-//catches ctrl+c event
-process.on('SIGINT', cleanUpResources);
-
-// catches "kill pid" (for example: nodemon restart)
-process.on('SIGUSR1', cleanUpResources);
-process.on('SIGUSR2', cleanUpResources);
-
-//catches uncaught exceptions
-process.on('uncaughtException', cleanUpResources);
-*/
